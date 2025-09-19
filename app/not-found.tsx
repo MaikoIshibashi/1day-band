b8a2912d2e9f0dea92cca60e3191547d0d73c160
石橋舞子 <ishibashimaiko@Myais-Mac.local>
2025-09-19 20:55:05 +0900

    fix: add not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,27 @@
+// app/not-found.tsx
 export default function NotFound() {
   return (
-    <section
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#000",
-        color: "white",
-        textAlign: "center",
-      }}
-    >
-      <h1 style={{ fontSize: "2rem", color: "var(--color-accent)" }}>
-        404 - ページが見つかりません
-      </h1>
-    </section>
+    <html>
+      <body
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#000",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        <div>
+          <h1 style={{ fontSize: "2rem", color: "var(--color-accent)" }}>
+            404 - ページが見つかりません
+          </h1>
+          <p style={{ marginTop: "1rem", color: "gray" }}>
+            入力したURLが間違っているか、すでに削除された可能性があります。
+          </p>
+        </div>
+      </body>
+    </html>
   );
 }
