1f3e9ecd28bef38c3c456bf222c605f7f18d7128
石橋舞子 <ishibashimaiko@Myais-Mac.local>
2025-09-19 21:11:34 +0900

    fix: replace <a> with <Link>

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,25 @@
 // app/not-found.tsx
 export default function NotFound() {
   return (
-    <html>
-      <body
-        style={{
-          minHeight: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#000",
-          color: "white",
-          textAlign: "center",
-        }}
-      >
-        <div>
-          <h1 style={{ fontSize: "2rem", color: "var(--color-accent)" }}>
-            404 - ページが見つかりません
-          </h1>
-          <p style={{ marginTop: "1rem", color: "gray" }}>
-            入力したURLが間違っているか、すでに削除された可能性があります。
-          </p>
-        </div>
-      </body>
-    </html>
+    <section
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#000",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
+      <div>
+        <h1 style={{ fontSize: "2rem", color: "var(--color-accent)" }}>
+          404 - ページが見つかりません
+        </h1>
+        <p style={{ marginTop: "1rem", color: "gray" }}>
+          入力したURLが間違っているか、すでに削除された可能性があります。
+        </p>
+      </div>
+    </section>
   );
 }
